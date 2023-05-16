1c477857bfb4efc53792b6ae17aa9d4c214df2c2
shubham-root <shubham.root@gmail.com>
2023-05-16 18:43:20 +0530

    dependencies changed to use from deno land

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export type { OnMessageCallback, SymbolDsl, PrimitiveTypes, ComponentTypes, Property, Schema } from "/Users/shubham/mayahq/pac-runtime/mod.ts";
-export { Symbol, TypedInput, exceptions } from "/Users/shubham/mayahq/pac-runtime/mod.ts"
-export {Runtime} from "/Users/shubham/mayahq/pac-runtime/src/runtime/runtime.ts"
+export type { OnMessageCallback, SymbolDsl, PrimitiveTypes, ComponentTypes, Property, Schema } from "https://deno.land/x/mayalabs_runtime/mod.ts";
+export { Symbol, TypedInput, exceptions } from "https://deno.land/x/mayalabs_runtime/mod.ts"
+export {Runtime} from "https://deno.land/x/mayalabs_runtime/src/runtime/runtime.ts"
 
 export {default as puppeteer} from 'https://deno.land/x/puppeteer@16.2.0/mod.ts';
